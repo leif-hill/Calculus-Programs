6f10b8650aad2cc2ca7f40ca186b11710d0903c2
Leif Hill <leifhill.business@gmail.com>
2024-04-04 19:49:06 -0700

    Add Newton's Method v2

---
+++ Newton's Method/newtonsmethod_v2.py
@@ -0,0 +1,33 @@
+import math
+
+def root(num, initial_guess):
+    approximated = False
+    iteration = 0
+
+# checks for and prevents divide by zero error
+    if (initial_guess**2 - num == 0):
+            initial_guess += 1
+            print("Error: Results in zero, adding one to initial iteration.")
+
+    x = initial_guess
+
+#loops until sqrt is found
+    while not approximated:
+
+    #checks if iteration value matches real sqrt
+        if (x == math.sqrt(num)): 
+            approximated = True
+            print("Finished.")
+
+        else:
+            x = x - ((x**2 - num)/(2*x)) #Newton's Method
+            iteration += 1
+
+            percent_diff = (abs(x - math.sqrt(num)) / ((math.sqrt(num)+x)/2) * 100.0) #Finds percent difference between iteration and actual root
+
+            print("*********************")
+            print("Iteration #{}".format(iteration), x)
+            print("Absolute Error:{}".format((math.sqrt(num)-x)), "Approx. ({}%)".format(round(percent_diff, 2)))
+
+root(5, 2)
+
